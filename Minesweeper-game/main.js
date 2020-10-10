fd04f54ce3f5a3af123a2fbcd32ae885475605be
HeeyeonJeong <junghy14971@gmail.com>
2020-10-10 23:18:57 +0900

    지뢰 심기

--- Minesweeper-game/main.js
+++ Minesweeper-game/main.js
@@ -13,7 +13,7 @@ document.querySelector("#exec").addEventListener("click",function () {
     console.log(후보);
 
     let 셔플 = [];
-    while (후보.length > 80) {
+    while (후보.length > hor*ver-mine) {
     let 이동값 = 후보.splice(Math.floor(Math.random() * 후보.length), 1)[0];
     셔플.push(이동값);
     }
@@ -33,6 +33,15 @@ document.querySelector("#exec").addEventListener("click",function () {
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
+    }
+
+    //테이블에 지뢰 심기
+    for(k=0; k< 셔플.length; k++){
+        let 세로 = Math.floor(셔플[k] / ver);
+        let 가로 = 셔플[k] % hor;
+        console.log(세로,가로);
+        tbody.children[세로].children[가로].innerHTML = 'X'
+        dataSet[세로][가로].innerHTML = 'X'
     }
     console.log(dataSet);
 })
