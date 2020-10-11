11a71bc8642b9cd5ae34aa71b3930ef8006a2caa
HeeyeonJeong <junghy14971@gmail.com>
2020-10-11 15:22:40 +0900

    깃발 꽂기

--- Minesweeper-game/main.js
+++ Minesweeper-game/main.js
@@ -1,3 +1,6 @@
+const tbody = document.querySelector("#table tbody");
+let dataSet = [];
+
 document.querySelector("#exec").addEventListener("click",function () {
     const hor = parseInt(document.querySelector("#hor").value);
     const ver = parseInt(document.querySelector("#ver").value);
@@ -10,7 +13,6 @@ document.querySelector("#exec").addEventListener("click",function () {
     .map(function (item, index) {
         return index;
     });
-    console.log(후보);
 
     let 셔플 = [];
     while (후보.length > hor*ver-mine) {
@@ -20,16 +22,34 @@ document.querySelector("#exec").addEventListener("click",function () {
     console.log(셔플);
 
     //지뢰 테이블 만들기
-    let dataSet = [];
-    const tbody = document.querySelector("#table tbody");
     tbody.innerHTML = '';
     for(i = 0; i < hor; i++){
         let arr = [];
-        let tr = document.createElement('tr');
+        const tr = document.createElement('tr');
         dataSet.push(arr);
         for(j = 0; j < ver; j++){
             arr.push(1);
-            let td = document.createElement('td');
+            const td = document.createElement('td');
+            td.addEventListener("contextmenu",function (e) {
+                e.preventDefault();
+                //몇번째 줄, 칸인지 확인하기, 깃발 꽂기
+                let 부모tr = e.currentTarget.parentNode;
+                let 부모tbody = e.currentTarget.parentNode.parentNode;
+                let 줄 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
+                let 칸 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
+                console.log(줄, 칸, e.currentTarget);
+                if(e.currentTarget.textContent === "" || e.currentTarget.textContent === "X"){
+                    e.currentTarget.textContent = "!";
+                }else if(e.currentTarget.textContent === "!"){
+                    e.currentTarget.textContent = "?";
+                }else if(e.currentTarget.textContent = "?"){
+                    if(dataSet[칸][줄] === "X"){
+                        e.currentTarget.textContent = "X";
+                    }else if(dataSet[칸][줄] === 1) {
+                        e.currentTarget.textContent = "";
+                    } 
+                }
+            })
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
@@ -41,7 +61,6 @@ document.querySelector("#exec").addEventListener("click",function () {
         let 가로 = 셔플[k] % hor;
         console.log(세로,가로);
         tbody.children[세로].children[가로].innerHTML = 'X'
-        dataSet[세로][가로].innerHTML = 'X'
+        dataSet[세로][가로] = 'X'
     }
-    console.log(dataSet);
 })
