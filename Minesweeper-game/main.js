9356d9a03a4d3f3f42985c838230d1e61d5694ba
HeeyeonJeong <junghy14971@gmail.com>
2020-10-12 19:22:44 +0900

    주변 8칸 open

--- Minesweeper-game/main.js
+++ Minesweeper-game/main.js
@@ -1,12 +1,10 @@
 const tbody = document.querySelector("#table tbody");
-let dataSet = [];
-
 
 document.querySelector("#exec").addEventListener("click",function () {
+    let dataSet = [];
     const hor = parseInt(document.querySelector("#hor").value);
     const ver = parseInt(document.querySelector("#ver").value);
     const mine = parseInt(document.querySelector("#mine").value);
-    console.log(hor,ver,mine);
 
     //지뢰 위치 랜덤 뽑기
     let 후보 = Array(hor*ver)
@@ -20,7 +18,6 @@ document.querySelector("#exec").addEventListener("click",function () {
     let 이동값 = 후보.splice(Math.floor(Math.random() * 후보.length), 1)[0];
     셔플.push(이동값);
     }
-    console.log(셔플);
 
     //지뢰 테이블 만들기
     tbody.innerHTML = '';
@@ -29,7 +26,7 @@ document.querySelector("#exec").addEventListener("click",function () {
         const tr = document.createElement('tr');
         dataSet.push(arr);
         for(j = 0; j < ver; j++){
-            arr.push(1);
+            arr.push(0);
             const td = document.createElement('td');
             //우클릭시,
             td.addEventListener("contextmenu",function (e) {
@@ -39,7 +36,6 @@ document.querySelector("#exec").addEventListener("click",function () {
                 let 부모tbody = e.currentTarget.parentNode.parentNode;
                 let 줄 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
                 let 칸 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
-                console.log(줄, 칸, e.currentTarget);
                 //우클릭시, 두번 - 물음표, 세번 - 원상복구
                 if(e.currentTarget.textContent === "" || e.currentTarget.textContent === "X"){
                     e.currentTarget.textContent = "!";
@@ -53,8 +49,9 @@ document.querySelector("#exec").addEventListener("click",function () {
                     } 
                 }
             })
-            //클릭시, 터지거나 or 지뢰갯수
+            //클릭시, 터지거나 or 지뢰개수
             td.addEventListener("click",function(e){
+                e.currentTarget.classList.add("open");
                 let 부모tr = e.currentTarget.parentNode;
                 let 부모tbody = e.currentTarget.parentNode.parentNode;
                 let 줄 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
@@ -62,6 +59,7 @@ document.querySelector("#exec").addEventListener("click",function () {
                 if(dataSet[칸][줄] === "X"){
                     e.currentTarget.textContent = "펑";
                 }else{
+                    dataSet[칸][줄] = 1;
                     let 주변 = [dataSet[칸][줄-1], dataSet[칸][줄+1]];
                     if(dataSet[칸-1]){
                         주변 = 주변.concat([dataSet[칸-1][줄-1], dataSet[칸-1][줄], dataSet[칸-1][줄+1]]);
@@ -69,10 +67,35 @@ document.querySelector("#exec").addEventListener("click",function () {
                     if(dataSet[칸+1]){
                         주변 = 주변.concat([dataSet[칸+1][줄-1], dataSet[칸+1][줄], dataSet[칸+1][줄+1]]);
                     }
-                    console.log(주변)
-                    e.currentTarget.textContent = 주변.filter(value => value === "X").length
-                }
-            })
+                    let 주변지뢰개수 = 주변.filter(value => value === "X").length
+                    e.currentTarget.textContent = 주변지뢰개수
+                    //0 클릭시, 주변 8칸 open
+                    if(주변지뢰개수 === 0){
+                        let 주변칸 = [tbody.children[칸].children[줄-1],tbody.children[칸].children[줄+1]];
+                        if(tbody.children[칸-1]){
+                            주변칸 = 주변칸.concat([tbody.children[칸-1].children[줄-1],
+                                tbody.children[칸-1].children[줄],
+                                tbody.children[칸-1].children[줄+1]])
+                        }
+                        if(tbody.children[칸+1]){
+                            주변칸 = 주변칸.concat([tbody.children[칸+1].children[줄-1],
+                                tbody.children[칸+1].children[줄],
+                                tbody.children[칸+1].children[줄+1]])
+                        }
+                        주변칸.filter(function(v){
+                            return !!v;
+                        }).forEach(function (옆칸) {
+                            let 부모tr = 옆칸.parentNode;
+                            let 부모tbody = 옆칸.parentNode.parentNode;
+                            let 옆칸칸 = Array.prototype.indexOf.call(부모tr.children, 옆칸);
+                            let 옆칸줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);   
+                            if(dataSet[옆칸줄][옆칸칸] !==1){
+                                옆칸.click();
+                            }         
+                        });
+                        }
+                    }
+                })
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
@@ -82,7 +105,6 @@ document.querySelector("#exec").addEventListener("click",function () {
     for(k=0; k< 셔플.length; k++){
         let 세로 = Math.floor(셔플[k] / ver);
         let 가로 = 셔플[k] % hor;
-        console.log(세로,가로);
         tbody.children[세로].children[가로].innerHTML = 'X'
         dataSet[세로][가로] = 'X'
     }
