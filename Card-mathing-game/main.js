cc3648297b33c5d1d83dae58c5e523205aeb55d8
HeeyeonJeong <junghy14971@gmail.com>
2020-10-17 21:04:48 +0900

    카드 짝 맞추기, 시간 체크

--- Card-mathing-game/main.js
+++ Card-mathing-game/main.js
@@ -1,8 +1,15 @@
+const wrapper =document.querySelector("#wrapper");
+const gameSet =document.querySelector("#wrapper").gameSet;
+
 let 가로 = 4;
 let 세로 = 3;
 let 색상 = ["red","yellow","green","orange","blue","pink","red","yellow","green","orange","blue","pink"];
 let 랜덤색상 = [];
 let 클릭플래그 = true; 
+let 클릭카드 = [];
+let 완성카드 = [];
+let 시작시간;
+let 끝시간;
 
 //색상 랜덤 넣기
 for(let i=0; 색상.length > 0; i++){
@@ -29,13 +36,35 @@ function 카드세팅(가로,세로){
         //카드 뒤집기
         (function(c){
             c.addEventListener("click",function(){
-                if(클릭플래그){
+                if(클릭플래그 && !완성카드.includes(c)){
                     c.classList.toggle("flipped");
+                    클릭카드.push(c);
+                    if(클릭카드.length === 2){
+                        if(클릭카드[0].querySelector(".card-back").style.backgroundColor === 클릭카드[1].querySelector(".card-back").style.backgroundColor){
+                            완성카드.push(클릭카드[0],클릭카드[1]);
+                            클릭카드 = [];
+                            if(완성카드.length === 12){
+                                끝시간 = new Date();
+                                wrapper.classList.add("gameSet");
+                                const desc = document.createElement("p");
+                                document.body.appendChild(desc);
+                                desc.innerHTML = `${(끝시간 - 시작시간)/1000}초!`
+                            }
+                        }else{
+                            setTimeout(() => {
+                                클릭카드[0].classList.remove("flipped");
+                                클릭카드[1].classList.remove("flipped");
+                                클릭플래그 = true;
+                                클릭카드 = [];    
+                            }, 500);
+                        }
+                    }
                 }
             });        
         })(card);
-        document.body.appendChild(card);
+        wrapper.appendChild(card);
     }
+
     //카드 하나씩 뒤집기
     document.querySelectorAll(".card").forEach(function(card, index){
         setTimeout(() => {
@@ -48,6 +77,7 @@ function 카드세팅(가로,세로){
             card.classList.remove("flipped");
         });
         클릭플래그 = true;
+        시작시간 = new Date();
     },3000);
 };
 카드세팅(가로,세로);
